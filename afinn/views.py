7fcf9ff70f809a7f02e1fa1b26bad958859866ad
steve yeago <subsume@gmail.com>
2012-02-07 19:04:10 -0500

    Derp

--- afinn/views.py
+++ afinn/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from afinn.helpers import get_sentiment
 
-def sentiment():
+def sentiment(request):
     text = request.GET.get('text') or request.POST.get('text')
     return HttpResponse(get_sentiment(text))
